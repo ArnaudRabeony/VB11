24d91527e2bd68b59b711a165349ffee663a012f
karamiAli <ali_92000@hotmail.fr>
2016-04-30 11:47:37 +0200

    raz form contact

--- sendemail.php
+++ sendemail.php
@@ -1,28 +1,16 @@
 <?php
+$name       = @trim(stripslashes($_POST['name'])); 
+$from       = @trim(stripslashes($_POST['email'])); 
+$subject    = @trim(stripslashes($_POST['subject'])); 
+$message    = @trim(stripslashes($_POST['message'])); 
+$to   		= 'karami.pro@gmail.com, ali_92000@hotmail.fr';
+ 
+$headers = 'From: '.$name.' <'.$from.'>'. "\r\n" .
+		   'Reply-To: <'.$from.'>'. "\r\n" .
+		   'Subject: '.$subject. "\r\n" .
+		   'X-Mailer: PHP/' . phpversion();
 
-if (isset($_POST) && isset($_POST['nom']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['message']) ) {extract($_POST);
-$date = date("d-m-Y");
-$heure = date("H:i");
+mail($to, $subject, $message, $headers);
 
-if (!empty($nom) && !empty($email) && !empty($message) ) {
-	$message=str_replace("\'", "'", $message);
-	$destinataire="karami.pro@gmail.com";
-	$sujet="Formulaire de contact - www.voyageauboutdela11.fr";
-	$message="Un nouveau message est arrivé à $heure le $date : \n
-	Nom : $nom \n
-	Email : $email \n
-	Objet : $rappel \n
-	Message : $message";
-
-			$entete="From: $nom \n Répondre à  : $email";
-			mail($destinataire, $sujet, $message, $entete);
-		  
-		}
-
-		else {
-			echo "Vous n'avez pas rempli tous les champs";
-		}
-
-}
-
-?>
+die;
+ ?>
